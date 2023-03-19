c4e3d9bc0e670a7d4022e0aa9fdd6010b8472506
ahmednadr <ahmednader.an5@gmail.com>
2023-03-19 17:00:32 +0200

    edit gen to load model

--- llama/generation.py
+++ llama/generation.py
@@ -3,16 +3,54 @@
 
 from typing import List
 
+
 import torch
+import time
+import json
+from pathlib import Path
 
 from llama.tokenizer import Tokenizer
-from llama.model import Transformer
+from llama.model import Transformer , ModelArgs
 
 
 class LLaMA:
-    def __init__(self, model: Transformer, tokenizer: Tokenizer):
+    # def __init__(self, model: Transformer, tokenizer: Tokenizer):
+    #     self.model = model
+    #     self.tokenizer = tokenizer
+    
+    def __init__(
+    self,
+    ckpt_dir: str,
+    tokenizer_path: str,
+    local_rank: int,
+    world_size: int,
+    max_seq_len: int,
+    max_batch_size: int,
+    ) :
+        start_time = time.time()
+        checkpoints = sorted(Path(ckpt_dir).glob("*.pth"))
+        assert world_size == len(
+            checkpoints
+        ), f"Loading a checkpoint for MP={len(checkpoints)} but world size is {world_size}"
+        ckpt_path = checkpoints[local_rank]
+        print("Loading")
+        checkpoint = torch.load(ckpt_path, map_location="cpu")
+        with open(Path(ckpt_dir) / "params.json", "r") as f:
+            params = json.loads(f.read())
+
+        model_args: ModelArgs = ModelArgs(
+            max_seq_len=max_seq_len, max_batch_size=max_batch_size, **params
+        )
+        tokenizer = Tokenizer(model_path=tokenizer_path)
+        model_args.vocab_size = tokenizer.n_words
+        torch.set_default_tensor_type(torch.cuda.HalfTensor)
+        model = Transformer(model_args)
+        torch.set_default_tensor_type(torch.FloatTensor)
+        model.load_state_dict(checkpoint, strict=False)
+
         self.model = model
         self.tokenizer = tokenizer
+        print(f"Loaded in {time.time() - start_time:.2f} seconds")
 
     def generate(
         self,
